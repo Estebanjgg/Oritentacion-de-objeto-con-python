a7f5b14193277014eeaea902b546211d44e544a2
Esteban Jose Gonzalez Gomez <91167097+Estebanjgg@users.noreply.github.com>
2022-08-19 21:44:06 -0300

    trabajando con get e set

--- conta.py
+++ conta.py
@@ -2,17 +2,33 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Contruindo objetos ... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
         
     def extrato(self):
-        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
     
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
     
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
     
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
+        
+        
