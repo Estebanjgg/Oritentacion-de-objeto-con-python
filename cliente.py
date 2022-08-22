b8a524a1e919a346f1f01dd861784459a3cc902b
Esteban <esteban030991@gmail.com>
2022-08-22 20:15:50 -0300

    get e setter

--- cliente.py
+++ cliente.py
@@ -1,7 +1,14 @@
 class Cliente:
 
     def __init__(self, nome):
-        self.nome = nome
+        self.__nome = nome
 
-    def get_nome(self):
-        return self.get_nome.title()
+    @property
+    def nome(self):
+        print("chamando @property nome()")
+        return self.__nome.title()
+
+    @nome.setter
+    def nome(self, nome):
+        print("chamando setter nome()")
+        self.__nome = nome
