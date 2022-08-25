c6ef1de81b282ff6871d42d76d3e5f7d598b2e7a
Esteban Jose Gonzalez Gomez <91167097+Estebanjgg@users.noreply.github.com>
2022-08-24 23:43:37 -0300

    trabajando con decoration

--- conta.py
+++ conta.py
@@ -6,23 +6,34 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+
         
     def extrato(self):
         print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
     
     def deposita(self, valor):
         self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
     
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
     
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
-    def get_saldo(self):
+
+    @property
+    def saldo(self):
         return self.__saldo
 
-    def get_titular(self):
+    @property
+    def titular(self):
         return self.__titular
 
     @property
@@ -32,5 +43,11 @@ class Conta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
-        
-        
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigo_banco():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
