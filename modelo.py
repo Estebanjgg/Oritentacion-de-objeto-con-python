d468bd7ccc8119e1ea168cd1207fc8ac3f2505fc
Esteban <esteban030991@gmail.com>
2022-09-04 14:46:59 -0300

    modelo

--- modelo.py
+++ modelo.py
@@ -19,24 +19,34 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def __str__(self):
+       return f'{self._nome} - {self.ano}  - {self._likes} Likes'
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+       return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+      return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_likes()
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
-
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
+
+
