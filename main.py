c40e6c1d86097897f5accfe935fdc27c8a4ea51f
Esteban <esteban030991@gmail.com>
2022-09-06 22:10:21 -0300

    url

---
+++ main.py
@@ -0,0 +1,2 @@
+url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+print(url)
--- modelo.py
+++ modelo.py
@@ -38,15 +38,47 @@ class Serie(Programa):
     def __str__(self):
       return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+    def __len__(self):
+        return len(self._programas)
+
+   
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_likes()
 atlanta = Serie('atlanta', 2018, 2)
+tmpe = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+vingadores.dar_likes()
+tmpe.dar_likes()
+tmpe.dar_likes()
+tmpe.dar_likes()
+tmpe.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+atlanta.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, tmpe]
+playlist_finde_de_semana = Playlist('find de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho do playlist: {len(playlist_finde_de_semana)}')
+
+print(playlist_finde_de_semana[0])
+
+
+for programa in playlist_finde_de_semana:
     print(programa)
 
 
