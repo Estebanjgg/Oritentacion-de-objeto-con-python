f7c1c882e1cb97fd2838119dfff584da7ab32fda
Esteban <esteban030991@gmail.com>
2022-09-21 21:36:10 -0300

    url

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
-print(url)
+url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+parame_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parame_busca)
+indice_valor = indice_parametro + len(parame_busca) + 1
+indice_e_comercial = url_parametros.find('&')
+valor = url_parametros[indice_valor:indice_e_comercial]
+print(valor) 
